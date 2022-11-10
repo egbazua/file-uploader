abca64e48a7c116f20f46e587837f98ee8933184
Enrique Bazúa <enriquegbazua@gmail.com>
2022-11-09 22:33:15 -0700

    creating a form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,26 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Container,
+} from '@chakra-ui/react';
+
 const App = () => {
   return (
-    <div>App</div>
+    <ChakraProvider>
+      <Center h='100vh' bg='black' color='white'>
+        <Container>
+          <FormControl>
+            <FormLabel>Title</FormLabel>
+            <Input type='text' />
+            <FormHelperText>Enter your title.</FormHelperText>
+          </FormControl>
+        </Container>
+      </Center>
+    </ChakraProvider>
   )
 }
 
