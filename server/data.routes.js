0f0ac21a83f41a464d08e3e649c389fdbe4969ea
Enrique Bazúa <enriquegbazua@gmail.com>
2022-11-11 09:57:05 -0700

    defining the routes

--- server/data.routes.js
+++ server/data.routes.js
@@ -3,4 +3,11 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/', (req, res) => res.send('Welcome to the server!'));
+router.post('/upload', (req, res) => {
+
+    console.log(req.files);
+    res.send('File uploaded!');
+});
+
 module.exports = router;
