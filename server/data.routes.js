a247e12ba368d47793610e4e4de158db3224532e
Enrique Bazúa <enriquegbazua@gmail.com>
2022-11-11 15:13:40 -0700

    adding cors

--- server/data.routes.js
+++ server/data.routes.js
@@ -5,7 +5,6 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome to the server!'));
 router.post('/upload', (req, res) => {
-
     console.log(req.files);
     res.send('File uploaded!');
 });
--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const dataRoutes = require('./data.routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: './files'
-}))
+}));
+
+app.use(dataRoutes);
 
 app.listen(3004);
 
