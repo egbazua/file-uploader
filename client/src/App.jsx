ab8c067b47f0f0914f2aa4bb3158fd0b18cf52e1
Enrique Bazúa <enriquegbazua@gmail.com>
2022-11-11 09:33:25 -0700

    configuring onChange event

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Center } from '@chakra-ui/react';
 import {
@@ -11,6 +12,12 @@ import {
 } from '@chakra-ui/react';
 
 const App = () => {
+
+  const [post, setPost] = useState({
+    title: '',
+    data: null
+  })
+
   return (
     <ChakraProvider>
       <Center h='100vh' bg='black' color='white'>
@@ -18,11 +25,11 @@ const App = () => {
         <Heading as='h1' mb='4' textAlign='center' >File Uploader</Heading>
           <FormControl>
             <FormLabel>Title</FormLabel>
-            <Input type='text' />
+            <Input type='text' onChange={ e => setPost({ ...post, title: e.target.value })} />
             <FormHelperText>Enter your title.</FormHelperText>
-            <Input type='file' mt='3' />
+            <Input type='file' mt='3' name='data' onChange={ e => setPost({ ...post, data: e.target.files[0] })} />
             <FormHelperText>Upload your file.</FormHelperText>
-            <Button colorScheme='teal' type='submit' mt='3' >Submit</Button>
+            <Button colorScheme='teal' type='submit' mt='3'>Submit</Button>
           </FormControl>
         </Container>
       </Center>
