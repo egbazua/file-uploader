1e383da097df380304b7605a463d6802831f4fad
Enrique Bazúa <enriquegbazua@gmail.com>
2022-11-11 15:55:33 -0700

    adding uploadFile to the route

--- server/data.routes.js
+++ server/data.routes.js
@@ -1,11 +1,18 @@
 
 const { Router } = require('express');
+const { uploadFile } = require('./config');
 
 const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome to the server!'));
-router.post('/upload', (req, res) => {
-    console.log(req.files);
+
+router.post('/upload', async(req, res) => {
+
+    console.log(req.files['data'].tempFilePath);
+
+    const result = await uploadFile(req.files['data'].tempFilePath);
+    console.log(result);
+    
     res.send('File uploaded!');
 });
 
